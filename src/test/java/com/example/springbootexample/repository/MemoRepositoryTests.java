10df7be3834ff25b70c4d22a5c7a81d4115df9de
kim hyun sik <devconf5296@gmail.com>
2021-07-22 15:35:48 +0900

    Jpa 페이징 정렬하기

--- src/test/java/com/example/springbootexample/repository/MemoRepositoryTests.java
+++ src/test/java/com/example/springbootexample/repository/MemoRepositoryTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
@@ -88,5 +89,18 @@ public class MemoRepositoryTests {
         for(Memo memo:result.getContent()){
             System.out.println(memo);
         }
+    }
+
+    @Test
+    public void testSort(){
+        Sort sort1 = Sort.by("mno").descending();
+
+        Pageable pageable = PageRequest.of(0,10,sort1);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+        });
     }
 }
