051149508f9cadcc317268d6022b177e289b06de
kim hyun sik <devconf5296@gmail.com>
2021-07-28 20:18:23 +0900

    querydsl을 이용한 단일항목 쿼리

--- src/test/java/com/example/springbootexample/repository/GuestbookRepositoryTests.java
+++ src/test/java/com/example/springbootexample/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package com.example.springbootexample.repository;
 
 import com.example.springbootexample.entity.GuestBook;
+import com.example.springbootexample.entity.QGuestBook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -39,5 +46,20 @@ public class GuestbookRepositoryTests {
 
             guestBookRepository.save(guestBook);
         }
+    }
+
+    @Test
+    public void testQuery1(){
+        Pageable pageable= PageRequest.of(0,10, Sort.by("gno").descending());
+        QGuestBook qGuestBook = QGuestBook.guestBook;
+        String keyword = "1";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expression = qGuestBook.title.contains(keyword);
+        builder.and(expression);
+        Page<GuestBook> result = guestBookRepository.findAll(builder,pageable);
+
+        result.stream().forEach(guestBook -> {
+            System.out.println(guestBook);
+        });
     }
 }
