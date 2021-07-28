207e31ec8d073ea7a68d6ae33f71ea624ac1ba22
kim hyun sik <devconf5296@gmail.com>
2021-07-28 20:32:01 +0900

    querydsl을 이용한 다중항목 쿼리

--- src/test/java/com/example/springbootexample/repository/GuestbookRepositoryTests.java
+++ src/test/java/com/example/springbootexample/repository/GuestbookRepositoryTests.java
@@ -62,4 +62,22 @@ public class GuestbookRepositoryTests {
             System.out.println(guestBook);
         });
     }
+
+    @Test
+    public void testQuery2(){
+        Pageable pageable= PageRequest.of(0,10, Sort.by("gno").descending());
+        QGuestBook qGuestBook = QGuestBook.guestBook;
+        String keyword = "1";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression exTitle = qGuestBook.title.contains(keyword);
+        BooleanExpression exContent = qGuestBook.content.contains(keyword);
+        BooleanExpression exAll = exTitle.or(exContent);
+        builder.and(exAll);
+        builder.and(qGuestBook.gno.gt(0L));
+        Page<GuestBook> result = guestBookRepository.findAll(builder,pageable);
+
+        result.stream().forEach(guestBook -> {
+            System.out.println(guestBook);
+        });
+    }
 }
